8f750835799c46a93dcf475a0aea3582bb587760
Huxley <huxhales@gmail.com>
2019-09-24 13:42:11 +1000

    :tada: erros

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const {tweet} = require('./twitCon')
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const bodyParser = require('body-parser')
+const {tweetText, errorResponse, check} = require('./twitCon');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-app.set('views', './views') 
-app.set('view engine', 'ejs')
+app.set('views', './views') ;
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res)  => {
     res.render('index')
-})
+});
 
 app.get('/help', (req, res) => {
     res.render('help')
-})
+});
 
 // This should be a Post request 
 app.get('/pleh', (req, res) => {
-    let content = req.query.content
-    // Test for content 
-    // console.log(content)
-    tweet(content)
-})
+    let content = req.query.content;
+    check(content, res);
+});
 
 app.get('/thanks', (req, res) => {
     res.render('thanks')
-})
+});
+
+app.get('/error', (req, res) => {
+    res.render('errors', {error: "There was a error"})
+});
 
 app.listen(port,() => {
     console.log('Server Live')
-})
+});
 
--- twitCon.js
+++ twitCon.js
@@ -10,16 +10,30 @@ const twitter = new Twitter({
    access_token_secret: process.env.ACTS
 });
 
-
-function tweet(content) {
-    if (filter.isProfane(content)){
-        twitter.post('statuses/update', {status: content}, function (error, twt, res){
-            console.log(error ? error : 'Tweet is out there');
-            console.log(twt, res)
-        })
+function check(content, res) {
+    let canPost = filter.isProfane(content);
+    if (!canPost){
+        tweetText(content, res)
+    } else {
+        errorResponse(res)
     }
 }
 
-module.exports = {
-    tweet
+
+function errorResponse(res) {
+    res.redirect('/error')
 }
+
+
+function tweetText(content, res) {
+    twitter.post('statuses/update', {status: content}, function (error, twt, res) {
+        console.log(error ? error : twt);
+    });
+    res.redirect('/thanks')
+}
+
+module.exports = {
+    tweetText,
+    errorResponse,
+    check
+};
