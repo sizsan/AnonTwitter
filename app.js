2696557048644ccfc47e890924c73ba07c20546d
sizsan <sizsan2001@gmail.com>
2019-09-24 10:15:50 +1000

    server created

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+app.set('views', './views') 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)  => {
+    res.render('index')
+})
+
+
+
+app.listen(port,() => {
+    console.log('Server Live')
+})
+
